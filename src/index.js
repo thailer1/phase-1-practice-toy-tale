3aabbf39c292b13ef9556f25e054b11f4cf6ce8d
Thailer <thailer.palmer@gmail.com>
2023-03-22 20:39:17 -0400

    lab complete

--- src/index.js
+++ src/index.js
@@ -13,3 +13,59 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+function fetchToys() {
+  fetch('http://localhost:3000/toys')
+	.then(response => response.json())
+	.then(toyArray => toyArray.forEach((toy) => makeToyCard(toy)))
+}
+
+fetchToys()
+
+function makeToyCard(toys) {
+    
+    const div = document.createElement("div")
+    div.className = "card"
+  
+    const h2 = document.createElement("h2")
+    h2.textContent = toys.name
+  
+    const image = document.createElement("img")
+    image.src = toys.image
+    image.className = "toy-avatar";
+    
+    const pLikes = document.createElement("p")
+    pLikes.textContent = toys.likes
+  
+    const likeButton = document.createElement("button")
+    likeButton.textContent = "like"
+    likeButton.className = "like-btn"
+    likeButton.setAttribute("id", toys.id)
+    likeButton.addEventListener("click", () => {
+      const newLikes = +pLikes.textContent + 1
+      pLikes.textContent = newLikes
+    })
+  
+    div.append(h2,image, pLikes, likeButton)
+
+    const toyCardContainer = document.querySelector("#toy-collection")
+
+    toyCardContainer.append(div)
+  
+}
+
+const newToyForm = document.querySelector(".add-toy-form")
+newToyForm.addEventListener("submit", handleAddNewToy)
+
+function handleAddNewToy(event) {
+  const newToyName = event.target["name"].value
+  const newToyImage = event.target["image"].value
+
+  const newToyCard = {
+    name: newToyName,
+    image: newToyImage,
+  }
+  event.preventDefault()
+  makeToyCard(newToyCard)
+
+  event.target.reset()
+}
